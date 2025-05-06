2dea1edfd8a1ca87c13bff5f7441d4eaa6d5518d
asifmmdv <mamedov_asif@icloud.com>
2025-05-06 16:26:51 +0400

    fixy

--- js/detail2.js
+++ js/detail2.js
@@ -145,6 +145,7 @@ for (let i = 0; i < rows; i++) {
 document.getElementById('cinemaContainer').innerHTML = cinemaHTML;
 
 const container = document.getElementById('cinemaContainer');
+const scrollZone = document.getElementById('cinemaScrollZone');
 const zoomInBtn = document.getElementById('zoomInBtn');
 const zoomOutBtn = document.getElementById('zoomOutBtn');
 
@@ -155,32 +156,36 @@ const maxScale = 1.5;
 
 function applyZoom() {
   container.style.transform = `scale(${scale})`;
+  
+  if (window.innerWidth <= 770 && scale > 0.5 || window.innerWidth > 771) {
+    scrollZone.style.overflowY = 'auto';
+  } else {
+    scrollZone.style.overflowY = 'hidden';
+  }
 }
 
 function checkResponsiveZoom() {
   if (window.innerWidth <= 770) {
     scale = 0.5;
-    applyZoom();
   } else {
-    scale = 1; 
-    applyZoom();
+    scale = 1;
   }
+  applyZoom();
 }
 
-zoomInBtn.onclick = function() {
+zoomInBtn.onclick = function () {
   if (scale < maxScale) {
-    scale += step;
+    scale = Math.min(scale + step, maxScale);
     applyZoom();
   }
 };
 
-zoomOutBtn.onclick = function() {
+zoomOutBtn.onclick = function () {
   if (scale > minScale) {
-    scale -= step;
+    scale = Math.max(scale - step, minScale);
     applyZoom();
   }
 };
 
 checkResponsiveZoom();
-
 window.onresize = checkResponsiveZoom;
