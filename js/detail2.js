7d143c690fcabd199df24d3c5b2d87fe3503a68d
asifmmdv <mamedov_asif@icloud.com>
2025-05-06 13:27:31 +0400

    sad

--- js/detail2.js
+++ js/detail2.js
@@ -72,17 +72,18 @@ const seatsPerRow = 17;
 let cinemaHTML = '';
 
 for (let i = 0; i < rows; i++) {
-    let rowHTML = '<div class="flex gap-1 items-center">';
+    let rowHTML = '<div class="flex gap-1 items-center w-full">';
 
     rowHTML += `
-        <span class="w-8 h-8 text-white font-bold flex items-center justify-center bg-black rounded-sm mr-8">
+        <span class="min-w-8 min-h-8 w-10 h-10 text-white  font-bold flex items-center justify-center desktop:ml-30 rounded-sm mr-20 text-xl">
             ${rows - i}
         </span>`;
 
     if (i === 0) {
         for (let j = 0; j < seatsPerRow; j++) {
             rowHTML += `
-                <span class="w-8 h-8 bg-gray-300 border border-gray-600 rounded-sm flex items-center justify-center cursor-pointer hover:bg-yellow-400">
+                <span class="min-w-8 min-h-8 w-10 h-10 flex rounded-lg duration-200 justify-center items-center text-xl cursor-pointer 
+                bg-[#C7C7C7] text-[#353535] border border-gray-600 hover:bg-gray-300">
                     ${j + 1}
                 </span>`;
         }
@@ -90,17 +91,19 @@ for (let i = 0; i < rows; i++) {
         let seatCount = 1;
         for (let j = 0; j < seatsPerRow; j++) {
             if (j >= seatsPerRow - 2) {
-                rowHTML += '<span class="w-8 h-8 opacity-0"></span>';
+                rowHTML += '<span class="min-w-8 min-h-8 w-10 h-10 opacity-0"></span>';
             } else if (j < 2) {
                 rowHTML += `
-                    <span class="w-8 h-8 bg-gray-300 border border-gray-600 rounded-sm flex items-center justify-center cursor-pointer hover:bg-yellow-400">
+                    <span class="min-w-8 min-h-8 w-10 h-10 flex rounded-lg duration-200 justify-center items-center text-xl cursor-pointer 
+                    bg-[#C7C7C7] text-[#353535] border border-gray-600 hover:bg-gray-300">
                         ${seatCount++}
                     </span>`;
             } else if (j >= 2 && j < 4) {
-                rowHTML += '<span class="w-8 h-8 opacity-0"></span>';
+                rowHTML += '<span class="min-w-8 min-h-8 w-10 h-10 opacity-0"></span>';
             } else {
                 rowHTML += `
-                    <span class="w-8 h-8 bg-gray-300 border border-gray-600 rounded-sm flex items-center justify-center cursor-pointer hover:bg-yellow-400">
+                    <span class="min-w-8 min-h-8 w-10 h-10 flex rounded-lg duration-200 justify-center items-center text-xl cursor-pointer 
+                    bg-[#C7C7C7] text-[#353535] border border-gray-600 hover:bg-gray-300">
                         ${seatCount++}
                     </span>`;
             }
@@ -109,12 +112,13 @@ for (let i = 0; i < rows; i++) {
         let seatCount = 1;
         for (let j = 0; j < seatsPerRow; j++) {
             if (j >= seatsPerRow - 2) {
-                rowHTML += '<span class="w-8 h-8 opacity-0"></span>';
+                rowHTML += '<span class="min-w-8 min-h-8 w-10 h-10 opacity-0"></span>';
             } else if (j < 4) {
-                rowHTML += '<span class="w-8 h-8 opacity-0"></span>';
+                rowHTML += '<span class="min-w-8 min-h-8 w-10 h-10 opacity-0"></span>';
             } else {
                 rowHTML += `
-                    <span class="w-8 h-8 bg-gray-300 border border-gray-600 rounded-sm flex items-center justify-center cursor-pointer hover:bg-yellow-400">
+                    <span class="min-w-8 min-h-8 w-10 h-10 flex rounded-lg duration-200 justify-center items-center text-xl cursor-pointer 
+                    bg-[#C7C7C7] text-[#353535] border border-gray-600 hover:bg-gray-300">
                         ${seatCount++}
                     </span>`;
             }
@@ -123,10 +127,11 @@ for (let i = 0; i < rows; i++) {
         let seatCount = 2;
         for (let j = 0; j < seatsPerRow; j++) {
             if (j < 5 || j >= seatsPerRow - 3) {
-                rowHTML += '<span class="w-8 h-8 opacity-0"></span>';
+                rowHTML += '<span class="min-w-8 min-h-8 w-10 h-10 opacity-0"></span>';
             } else {
                 rowHTML += `
-                    <span class="w-8 h-8 bg-gray-300 border border-gray-600 rounded-sm flex items-center justify-center cursor-pointer hover:bg-yellow-400">
+                    <span class="min-w-8 min-h-8 w-10 h-10 flex rounded-lg duration-200 justify-center items-center text-xl cursor-pointer 
+                    bg-[#C7C7C7] text-[#353535] border border-gray-600 hover:bg-gray-300">
                         ${seatCount++}
                     </span>`;
             }
